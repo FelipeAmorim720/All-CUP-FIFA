a2738f9e84f6970e7164aa37140aa15b7b3977c9
FelipeAmorim720 <felipe.reis@bandtec.com.br>
2021-05-29 03:45:49 -0300

    29-05-last

--- BD/Script/FIFA-Script.sql
+++ BD/Script/FIFA-Script.sql
@@ -11,6 +11,7 @@ create table jogadores(
 	dataNasc date
 );
 
+select * from jogadores;
 create table campeonatos(
 	idCampeonato int primary key auto_increment,
     nome varchar(40),
@@ -24,15 +25,17 @@ create table campeonatos(
 create table feed(
 	idInfo int primary key auto_increment,
     titulo varchar (45),
-    mensagem varchar(350)
+    mensagem varchar(350),
+    fkjogador int,
+    foreign key (fkjogador) references jogadores (idJogador)
 );
 
 insert into jogadores values 
-	(null,'Vitor Santos','+55 41 95874-8744','vitorsantosreis@gmail.com','PlayStation', '2003-04-03'),
-	(null,'Marcelo Batista','+55 61 94741-4412','marceloBatista@gmail.com','Xbox', '1998-07-30'),
-	(null,'Gabriela Barbosa','+55 11 94055-2451','gabrielabarbosa@gmail.com','Xbox', '2005-06-14'),
-	(null,'Felipe Amorim','+55 11 96019-6061','felipe.reis@bandtec.com','PlayStation', '2002-02-25'),
-    (null,'Maria Eduarda','+55 11 95785-8788','maria.eduarda@gmail.com','Playstation','2003-06-12');
+	(null,'Vitor Santos','vitorsantosreis@gmail.com','153045','PlayStation', '2003-04-03'),
+	(null,'Marcelo Batista','marceloBatista@gmail.com','123456','Xbox', '1998-07-30'),
+	(null,'Gabriela Barbosa','gabrielabarbosa@gmail.com','87654','Xbox', '2005-06-14'),
+	(null,'Felipe Amorim','felipe.reis@bandtec.com','6015','PlayStation', '2002-02-25'),
+    (null,'Maria Eduarda','maria.eduarda@gmail.com','1515','Playstation','2003-06-12');
     
 insert into  campeonatos values 
 	(null,'Ligas Nacionais','Mata-a-Mata','2300','8745','1'),
@@ -40,14 +43,23 @@ insert into  campeonatos values
 	(null,'eClub World Cup','Mata-a-Mata','3200','41455','3'),
 	(null,'Ultimate Team','Pontos Corridos','5500','4441','4');
     
-insert into feed values 
-	(null,'Curiosidades','Em 2017, FIFA entrou para os Recordes Mundiais do Guinness como franquia de jogo de esporte mais vendida do mundo.'),
-	(null,'Ultimas Noticias','ELECTRONIC ARTS anuncia expansão global multiplataforma do EA SPORTS FIFA'),
-	(null,'Eventos','RULEBREAKEARS dia 20/01'),
-	(null,'Ofertas e Promoções','FIFA 20 com promoção de 20%'),
+    insert into feed values 
+	(null,'Curiosidades','Em 2017, FIFA entrou para os Recordes Mundiais do Guinness como franquia de jogo de esporte mais vendida do mundo.','1'),
+	(null,'Ultimas Noticias','ELECTRONIC ARTS anuncia expansão global multiplataforma do EA SPORTS FIFA','2'),
+	(null,'Eventos','RULEBREAKEARS dia 20/01', '4'),
+	(null,'Ofertas e Promoções','FIFA 20 com promoção de 20%','5'),
 	(null,'Atualizações','A partir do dia 10/03, EA iniciará a distribuição das novas versões
-dos jogadores, referentes a atletas que cresceram muito de dentro dos campos reais desde o lançamento do game (em outubro de 2020)');
+dos jogadores, referentes a atletas que cresceram muito de dentro dos campos reais desde o lançamento do game (em outubro de 2020)','3');
 
-    select * from jogadores inner join campeonatos on fkjogadores = idjogador;
+
+    select * from campeonatos;
     
-update jogadores set fkInfo = '1' where idJogador = '5';
+    SELECT 
+    nome,
+    tipo,
+	premiação,
+	pinCamp
+    FROM campeonatos;
+    
+    select * from jogadores inner join campeonatos on fkjogadores = idjogador;
+	update jogadores set fkInfo = '1' where idJogador = '5';
